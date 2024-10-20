40171e563c8e13c81887035d9a50319790cdef5e
Danial Jalaluddin <dan90@live.com.my>
2024-10-21 00:27:37 +0800

    update

---
+++ app.js
@@ -0,0 +1,29 @@
+// const people = [
+//     {
+//         "first":"Laurence",
+//         "last":"Svekis"
+//     },
+//     {
+//         "first":"Jack",
+//         "last":"Doe"
+//     }
+// ];
+
+// console.log(people);
+
+// people.forEach(person => {
+//     const temp = `${person.first}: ${person.last}`
+//     console.log(temp);
+// })
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// let i = 0;
+// while ( i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+console.log('Hello');
